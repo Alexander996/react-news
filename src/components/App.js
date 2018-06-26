61ed6680230e51db4f0f805f04f8ab19ec16836f
Alexander Smirnov <al.smirnov996@gmail.com>
2018-06-26 16:28:18 +0300

    Added user form

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 import ArticleList from './ArticleList'
+import UserForm from './UserForm'
 
 export default class App extends Component {
     render() {
         return (
             <div>
+                <UserForm/>
                 <ArticleList articles={this.props.articles}/>
             </div>
         )
---
+++ src/components/UserForm.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+
+export default class UserForm extends Component {
+    state = {
+        username: ''
+    };
+
+    render() {
+        return (
+            <div>
+                Name: <input type='text' value={this.state.username} onChange={this.handleUserChange}/>
+            </div>
+        )
+    }
+
+    handleUserChange = (ev) => {
+        if (ev.target.value.length > 5) return;
+
+        this.setState({
+            username: ev.target.value
+        })
+    }
+}
