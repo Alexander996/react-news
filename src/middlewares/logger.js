308aed240899253224e6954094201db21c7d5893
Alexander Smirnov <al.smirnov996@gmail.com>
2018-06-27 22:45:43 +0300

    Added logger middleware

---
+++ src/middlewares/logger.js
@@ -0,0 +1,6 @@
+export default store => next => action => {
+    console.log('State before:', store.getState());
+    console.log('Dispatching', action);
+    next(action);
+    console.log('State after:', store.getState());
+}
