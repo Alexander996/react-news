9b4dfad28abab6128f70ca916406aa0f1eb44090
Alexander Smirnov <al.smirnov996@gmail.com>
2018-07-02 18:12:30 +0300

    Added simple routing

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,26 @@ import ArticleList from './ArticleList'
 import UserForm from './UserForm'
 import Filters from './Filters'
 import Counter from './Counter'
+import {HashRouter as Router, Route, NavLink} from 'react-router-dom'
 
 export default class App extends Component {
     render() {
         return (
-            <div>
-                <Counter/>
-                <hr/>
-                <UserForm/>
-                <Filters/>
-                <ArticleList/>
-            </div>
+            <Router>
+                <div>
+                    <div>
+                        <h2>Main menu</h2>
+                        <div><NavLink activeStyle={{color: 'red'}} to='/counter'>Counter</NavLink></div>
+                        <div><NavLink activeStyle={{color: 'red'}} to='/filters'>Filters</NavLink></div>
+                        <div><NavLink activeStyle={{color: 'red'}} to='/articles'>Articles</NavLink></div>
+                    </div>
+                    <UserForm/>
+                    <Route path='/counter' component={Counter}/>
+                    <hr/>
+                    <Route path='/filters' component={Filters}/>
+                    <Route path='/articles' component={ArticleList}/>
+                </div>
+            </Router>
         )
     }
 }
